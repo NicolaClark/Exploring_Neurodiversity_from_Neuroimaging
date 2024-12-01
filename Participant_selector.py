aa736b8ba98bb00b4b8e286c882bce6763b3f91e
Nicola Clark <niccl@itu.dk>
2024-12-01 22:41:14 +0100

    Energy landscape cleaned up, added some better file structure

--- Participant_selector.py
+++ Participant_selector.py
@@ -1,39 +1,32 @@
 import pandas as pd
 import os
 
-
-
-
-output_dir = "selected_participants"
+#read in files and specify paths
+output_dir = "data/selected_participants"
 os.makedirs(output_dir, exist_ok=True)
 
-abide_input_file = "ABIDE_phenotypic_NYU.csv"
-adhd200_input_file = "adhd200_phenotypics.tsv"
-
-
-
+abide_input_file = "data/phenotypic/ABIDE_phenotypic_NYU.csv"
+adhd200_input_file = "data/phenotypic/adhd200_phenotypics.tsv"
 
 abide_df = pd.read_csv(abide_input_file)
 adhd200_df = pd.read_csv(adhd200_input_file, sep='\t')
 
-# Define your conditions
-# Example: Selecting rows where AGE_AT_SCAN is greater than 18 and SEX is 'Male'
+# Define conditions
+# Selecting rows where age is between 5 and 13 and various IQs are 80-140
 filtered_abide_df = abide_df[
     (5 < abide_df['AGE_AT_SCAN']) & (abide_df['AGE_AT_SCAN'] < 13) &
     (80 < abide_df['FIQ']) & (abide_df['FIQ'] < 140) &
     (80 < abide_df['VIQ']) & (abide_df['VIQ'] < 140) &
-    (80 < abide_df['PIQ']) & (abide_df['PIQ'] < 140) # Example condition
+    (80 < abide_df['PIQ']) & (abide_df['PIQ'] < 140) 
 ]
 
 filtered_adhd200_df = adhd200_df[
     (5 < adhd200_df['Age']) & (adhd200_df['Age'] < 13) &
     (80 < adhd200_df['Full4 IQ']) & (adhd200_df['Full4 IQ'] < 140) &
     (80 < adhd200_df['Verbal IQ']) & (adhd200_df['Verbal IQ'] < 140) &
-    (80 < adhd200_df['Performance IQ']) & (adhd200_df['Performance IQ'] < 140) # Example condition
+    (80 < adhd200_df['Performance IQ']) & (adhd200_df['Performance IQ'] < 140)
 ]
 
-
-#print(filtered_adhd200_df.columns.tolist())
 
 output_file1_1 = os.path.join(output_dir, "ABIDE_pure_ASD_phenotypic_NYU.csv")
 output_file1_2 = os.path.join(output_dir,"ADHD200_pure_ADHD_phenotypic_NYU.csv")
